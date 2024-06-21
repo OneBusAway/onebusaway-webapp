e7afa2fd22f519cde07556e35281f53e7b94e2ac
Aaron Brethorst <aaron@brethorsting.com>
2024-06-20 22:36:31 -0700

    Rearrange Header and layout a bit

--- src/components/navigation/Header.svelte
+++ src/components/navigation/Header.svelte
@@ -2,6 +2,8 @@
     import { PUBLIC_OBA_REGION_NAME } from '$env/static/public';
 </script>
 
-<div class='py-2 px-2'>
-    <h1 class='text-3xl font-semibold'>{PUBLIC_OBA_REGION_NAME}</h1>
+<div class='bg-blur-md bg-white/80 border-b border-neutral-300'>
+    <div class='py-2 px-2'>
+        <h1 class='text-3xl font-semibold'>{PUBLIC_OBA_REGION_NAME}</h1>
+    </div>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,10 @@
 	import Header from '../components/navigation/Header.svelte'
 </script>
 
-<div class='h-dvh w-full'>
-	<Header />
+<div class='h-dvh w-full relative'>
+	<div class='absolute top-0 left-0 right-0 z-40'>
+		<Header />
+	</div>
 	<div>
 		<slot></slot>
 	</div>
