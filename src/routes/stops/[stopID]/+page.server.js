0bf4cff3417024a0b98d1b7e640a96f1e42f01f5
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-08-21 23:39:35 +0300

    refactor: update arrivalsAndDeparturesResponse to use the sdk

--- src/routes/stops/[stopID]/+page.server.js
+++ src/routes/stops/[stopID]/+page.server.js
@@ -4,13 +4,13 @@ import arrivalDepartureAPI from '$lib/RestAPI/arrivalsAndDeparturesForStop.js';
 export async function load({ params }) {
 	const stopID = params.stopID;
 	const stopResponse = await stopAPI(stopID);
-	const stopBody = await stopResponse.json();
+
 	const arrivalsAndDeparturesResponse = await arrivalDepartureAPI(stopID);
-	const arrivalsAndDeparturesResponseJSON = await arrivalsAndDeparturesResponse.json();
+	const arrivalsAndDeparturesResponseJson = await arrivalsAndDeparturesResponse.json();
 
 	return {
 		stopID: params.stopID,
-		stopData: stopBody.data,
-		arrivalsAndDeparturesResponse: arrivalsAndDeparturesResponseJSON
+		stopData: stopResponse.data,
+		arrivalsAndDeparturesResponse: arrivalsAndDeparturesResponseJson
 	};
 }
