47ecf1d1db6362231c41f26166054b16f4dda913
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-08-23 01:52:36 +0300

    feat: Add stopsForLocation to use obaSD to fetch data

---
+++ src/lib/RestAPI/stops-for-location.js
@@ -0,0 +1,11 @@
+import oba from '../obaSdk';
+import { json } from '@sveltejs/kit';
+export default async function stopsForLocation(queryParams) {
+	const response = await oba.stopsForLocation.list(queryParams);
+
+	if (response.code !== 200) {
+		error(500, 'Unable to fetch stops-for-location.');
+	}
+
+	return json(response);
+}
