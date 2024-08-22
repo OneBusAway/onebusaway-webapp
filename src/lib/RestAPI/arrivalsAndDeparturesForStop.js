dc65dadd36e130972c5c94ea7b7184c8243e6e4a
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-08-23 01:51:39 +0300

    refactor: Use onebusaway sdk for API calls in arrivalsAndDeparturesForStop.js

--- src/lib/RestAPI/arrivalsAndDeparturesForStop.js
+++ src/lib/RestAPI/arrivalsAndDeparturesForStop.js
@@ -1,17 +1,8 @@
-import { error, json } from '@sveltejs/kit';
-import { PUBLIC_OBA_SERVER_URL as baseURL } from '$env/static/public';
-import { PRIVATE_OBA_API_KEY as apiKey } from '$env/static/private';
+import { json } from '@sveltejs/kit';
+import oba from '../obaSdk';
 
-import onebusaway from 'onebusaway-sdk';
-
-const oba = new onebusaway({
-	baseURL,
-	apiKey
-});
-
-export default async function (stopID) {
+export default async function arrivalsAndDeparturesForStop(stopID) {
 	const response = await oba.arrivalAndDeparture.list(stopID);
-
 	if (response.code !== 200) {
 		error(500, 'Unable to fetch arrivals-and-departures-for-stop.');
 	}
