3a43aace6b2e647da1db4fb31498262413809afc
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-08-23 01:53:26 +0300

    refactor: Update tripDetails.js to use obaSdk for retrieving trip details

--- src/lib/RestAPI/tripDetails.js
+++ src/lib/RestAPI/tripDetails.js
@@ -1,24 +1,10 @@
-import { error, json } from '@sveltejs/kit';
-import { PUBLIC_OBA_SERVER_URL as baseURL } from '$env/static/public';
-import { PRIVATE_OBA_API_KEY as apiKey } from '$env/static/private';
-
-export async function GET({ params, url }) {
-	const { tripId } = params;
-	const vehicleId = url.searchParams.get('vehicleId');
-	const serviceDate = url.searchParams.get('serviceDate');
-
-	let apiURL = `${baseURL}/api/where/trip-details/${tripId}.json?key=${apiKey}`;
-
-	if (vehicleId) apiURL += `&vehicleId=${vehicleId}`;
-	if (serviceDate) apiURL += `&serviceDate=${serviceDate}`;
-
-	const response = await fetch(apiURL);
-
-	if (!response.ok) {
+import oba from '../obaSdk';
+import { json } from '@sveltejs/kit';
+export default async function tripDetails(tripID, queryParams) {
+	const response = await oba.tripDetails.retrieve(tripID, queryParams);
+	if (response.code !== 200) {
 		error(500, 'Unable to fetch trip-details.');
-		return;
 	}
 
-	const data = await response.json();
-	return json(data);
+	return json(response);
 }
