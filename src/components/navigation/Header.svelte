a1879228a187ffbcc04462aac573a975191bac0b
tarunsinghofficial <tarunsinghwap7@gmail.com>
2024-08-23 21:02:54 +0530

    added the searchbar in navbar

--- src/components/navigation/Header.svelte
+++ src/components/navigation/Header.svelte
@@ -1,15 +1,68 @@
 <script>
-	import { PUBLIC_OBA_REGION_NAME, PUBLIC_OBA_LOGO_URL } from '$env/static/public';
+	import {
+		PUBLIC_OBA_REGION_NAME,
+		PUBLIC_OBA_LOGO_URL,
+		PUBLIC_OBA_SEARCH_ENABLED
+	} from '$env/static/public';
 	import ThemeSwitcher from '$lib/ThemeSwitch/ThemeSwitcher.svelte';
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	let searchQuery = '';
+
+	function handleSearch(event) {
+		if (event.key === 'Enter') {
+			dispatch('You searched for ', searchQuery);
+		}
+	}
 </script>
 
 <div
-	class="bg-blur-md flex items-center justify-between border-b border-neutral-300 bg-white/80 px-4 dark:bg-black dark:text-white"
+	class="flex items-center justify-between px-4 border-b bg-blur-md border-neutral-300 bg-white/80 dark:bg-black dark:text-white"
 >
-	<div class="px-2 py-2">
-		<a href="/">
-			<img src={PUBLIC_OBA_LOGO_URL} alt={PUBLIC_OBA_REGION_NAME} class="h-10 rounded-sm" />
-		</a>
+	<div class="flex items-center justify-center gap-4 px-2 py-2">
+		<div>
+			<a href="/">
+				<img src={PUBLIC_OBA_LOGO_URL} alt={PUBLIC_OBA_REGION_NAME} class="h-10 rounded-sm" />
+			</a>
+		</div>
+		{#if PUBLIC_OBA_SEARCH_ENABLED === 'true'}
+			<div class="max-w-md mx-auto">
+				<label
+					for="default-search"
+					class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label
+				>
+				<div class="relative">
+					<div class="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
+						<svg
+							class="w-4 h-4 text-gray-500 dark:text-gray-400"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 20 20"
+						>
+							<path
+								stroke="currentColor"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+							/>
+						</svg>
+					</div>
+					<input
+						type="search"
+						id="default-search"
+						class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 ps-10 dark:border-gray-600 dark:text-white dark:placeholder-gray-400"
+						placeholder="Search a stop or route"
+						required
+						bind:value={searchQuery}
+						on:keydown={handleSearch}
+					/>
+				</div>
+			</div>
+		{/if}
 	</div>
 	<div>
 		<ThemeSwitcher />
