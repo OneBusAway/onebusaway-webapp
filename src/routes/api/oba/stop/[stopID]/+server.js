5422d78585c3e395e4a84a39707766fdc962c5fa
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-08-24 17:36:50 +0300

    refactor: use sdk directly for api interactions

--- src/routes/api/oba/stop/[stopID]/+server.js
+++ src/routes/api/oba/stop/[stopID]/+server.js
@@ -1,8 +1,10 @@
+import oba, { handleOBAResponse } from '$lib/obaSdk';
+
 /** @type {import('./$types').RequestHandler} */
-
-import stop from '../../../../../lib/RestAPI/stop';
-
 export async function GET({ params }) {
 	const stopID = params.stopID;
-	return stop(stopID);
+
+	const response = await oba.stop.retrieve(stopID);
+
+	return handleOBAResponse(response, 'stop');
 }
