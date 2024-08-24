ead98f8c62000ecaec3de656ffda7a4e56f943ce
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-08-24 17:33:23 +0300

    refactor: use sdk directly for api interactions

--- src/routes/api/oba/shape/[shapeId]/+server.js
+++ src/routes/api/oba/shape/[shapeId]/+server.js
@@ -1,6 +1,9 @@
-import shape from '../../../../../lib/RestAPI/shape.js';
+import oba, { handleOBAResponse } from '$lib/obaSdk.js';
 
+/** @type {import('./$types').RequestHandler} */
 export async function GET({ params }) {
 	const shapeId = params.shapeId;
-	return shape(shapeId);
+	const response = await oba.shape.retrieve(shapeId);
+
+	return handleOBAResponse(response, 'shape');
 }
