46a33b1931ba046fe81b2cb856174bfb4ea7a6cc
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-08-24 17:28:40 +0300

    refactor: add handleOBAResponse function to handle response errors

--- src/lib/obaSdk.js
+++ src/lib/obaSdk.js
@@ -1,10 +1,19 @@
 import onebusaway from 'onebusaway-sdk';
 import { PUBLIC_OBA_SERVER_URL as baseURL } from '$env/static/public';
 import { PRIVATE_OBA_API_KEY as apiKey } from '$env/static/private';
+import { json } from '@sveltejs/kit';
 
 const oba = new onebusaway({
 	baseURL,
 	apiKey
 });
+
+export function handleOBAResponse(response, entityName) {
+	if (response.code !== 200) {
+		return error(500, `Unable to fetch ${entityName}.`);
+	}
+
+	return json(response);
+}
 
 export default oba;
