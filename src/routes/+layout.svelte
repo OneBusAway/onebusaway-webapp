5b8202e66288ea6d275cffe1a9da87de97b69f81
tarunsinghofficial <tarunsinghwap7@gmail.com>
2024-09-03 12:06:19 +0530

    fix: header added to stop page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script>
+	import Header from '$components/navigation/Header.svelte';
 	import '../app.css';
 	import { config } from '@fortawesome/fontawesome-svg-core';
 	import '@fortawesome/fontawesome-svg-core/styles.css';
 	config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 </script>
 
-<div class="relative h-dvh w-full">
+<div class="h-dvh relative w-full">
+	<div class="absolute w-full">
+		<Header />
+	</div>
 	<div>
 		<slot></slot>
 	</div>
