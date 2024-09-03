8c11f9cd767fa6bab0f20aa7befdc5f4b2e26c70
Aaron Brethorst <aaron@brethorsting.com>
2024-09-02 19:44:12 -0700

    linter fixes

--- src/components/navigation/Header.svelte
+++ src/components/navigation/Header.svelte
@@ -33,7 +33,7 @@
 </script>
 
 <div
-	class="flex items-center justify-between px-4 border-b bg-blur-md border-neutral-300 bg-white/80 dark:bg-black dark:text-white"
+	class="bg-blur-md flex items-center justify-between border-b border-neutral-300 bg-white/80 px-4 dark:bg-black dark:text-white"
 >
 	<div class="flex items-center justify-center gap-4 px-2 py-2">
 		<div>
@@ -42,15 +42,15 @@
 			</a>
 		</div>
 		{#if PUBLIC_OBA_SEARCH_ENABLED === 'true'}
-			<div class="max-w-md mx-auto">
+			<div class="mx-auto max-w-md">
 				<label
 					for="default-search"
-					class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label
+					class="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white">Search</label
 				>
 				<div class="relative">
-					<div class="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
+					<div class="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3">
 						<svg
-							class="w-4 h-4 text-gray-500 dark:text-gray-400"
+							class="h-4 w-4 text-gray-500 dark:text-gray-400"
 							aria-hidden="true"
 							xmlns="http://www.w3.org/2000/svg"
 							fill="none"
@@ -68,7 +68,7 @@
 					<input
 						type="search"
 						id="search"
-						class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 ps-10 dark:border-gray-600 dark:placeholder-gray-400"
+						class="block w-full rounded-full border border-gray-300 bg-gray-50 p-2 ps-10 text-sm text-gray-900 dark:border-gray-600 dark:placeholder-gray-400"
 						placeholder="Search a stop or route"
 						required
 						bind:value={searchInput}
