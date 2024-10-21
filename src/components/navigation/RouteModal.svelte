4ecdeef243e9c7a48c2452e6d49a66108bec5def
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-10-21 09:18:05 +0300

    refactor: styling

--- src/components/navigation/RouteModal.svelte
+++ src/components/navigation/RouteModal.svelte
@@ -30,13 +30,13 @@
 	<div>
 		<div>
 			<div class="h-36 rounded-lg bg-[#1C1C1E] bg-opacity-80 p-4">
-				<h1 class="mb-1 text-2xl font-bold text-white">Route: {selectedRoute.shortName}</h1>
-				<h2 class="mb-1 text-xl text-white">{selectedRoute.longName}</h2>
+				<h1 class="mb-6 text-center text-2xl font-bold text-white">
+					Route: {selectedRoute.shortName}
+				</h1>
+				<h2 class="mb-6 text-center text-xl text-white">{selectedRoute.description}</h2>
 			</div>
 		</div>
-		<div>
-			<h2 class="ml-4 mt-4 text-xl text-white">Stops</h2>
-		</div>
+
 		<div class="scrollbar-hidden h-96 space-y-2 overflow-y-scroll rounded-lg">
 			<div>
 				{#each stops as stop}
