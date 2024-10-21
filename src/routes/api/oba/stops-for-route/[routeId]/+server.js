8720d4a0b5716993e4b5648a011acbbdbc6c30a9
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-10-21 08:30:38 +0300

    chore: format

--- src/routes/api/oba/stops-for-route/[routeId]/+server.js
+++ src/routes/api/oba/stops-for-route/[routeId]/+server.js
@@ -1,8 +1,7 @@
 import oba, { handleOBAResponse } from '$lib/obaSdk.js';
 
-
 export async function GET({ params }) {
-    const routeId = params.routeId;
-    const response = await oba.stopsForRoute.list(routeId);
-    return handleOBAResponse(response, 'stops-for-route');
+	const routeId = params.routeId;
+	const response = await oba.stopsForRoute.list(routeId);
+	return handleOBAResponse(response, 'stops-for-route');
 }
