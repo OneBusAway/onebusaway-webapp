9276155913f7f248edd08a96c2cc61b42e6d45b2
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-10-23 08:11:09 +0300

    feat: Add support for additional languages in i18n
    This commit adds support for the following languages in the i18n module:
    - Polish (pl)
    - Vietnamese (vi)
    - Tagalog (tl)
    - Somali (so)
    - Amharic (am)

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -1,11 +1,19 @@
 import { init, register, getLocaleFromNavigator } from 'svelte-i18n';
 
-register('en', () => import('./../locales/en.json'));
-register('es', () => import('./../locales/es.json'));
+
+register('en', () => import('./../locales/en.json'));  // English
+register('es', () => import('./../locales/es.json'));	// Spanish
+register('pl', () => import('./../locales/pl.json'));  // Polish
+register('vi', () => import('./../locales/vi.json'));  // Vietnamese
+register('tl', () => import('./../locales/tl.json'));  // Tagalog
+register('so', () => import('./../locales/so.json'));  // Somali
+register('am', () => import('./../locales/am.json'));  // Amharic
 
 const locale = getLocaleFromNavigator();
 
 const lang = locale ? locale.split('-')[0] : 'en';
+
+console.log(lang)
 
 init({
 	fallbackLocale: 'en',
