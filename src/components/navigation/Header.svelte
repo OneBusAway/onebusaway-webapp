97c179348fbd3a33ad26181fda210849dd18495b
tarunsinghofficial <tarunsinghwap7@gmail.com>
2024-10-23 11:59:17 +0530

    refactor: fixes previous header commits with new

--- src/components/navigation/Header.svelte
+++ src/components/navigation/Header.svelte
@@ -6,29 +6,9 @@
 	} from '$env/static/public';
 
 	import ThemeSwitcher from '$lib/ThemeSwitch/ThemeSwitcher.svelte';
-	import { createEventDispatcher } from 'svelte';
 	import { fly } from 'svelte/transition';
 
-	const dispatch = createEventDispatcher();
-
-	let searchInput = '';
 	let isMobileMenuOpen = false;
-
-	async function handleSearch() {
-		try {
-			const response = await fetch(`/api/oba/search?query=${encodeURIComponent(searchInput)}`);
-			const results = await response.json();
-			dispatch('searchResults', results);
-		} catch (error) {
-			console.error('Error fetching search results:', error);
-		}
-	}
-
-	const onHandleSearch = (event) => {
-		if (event.key === 'Enter') {
-			handleSearch();
-		}
-	};
 
 	let headerLinks = null;
 
@@ -46,9 +26,12 @@
 >
 	<div class="md:flex-none flex items-center justify-between flex-1">
 		<div class="md:w-auto flex justify-between w-full gap-4 px-2 py-2">
-			<div>
-				<a href="/">
+			<div class="gap-x-2 flex items-center justify-center">
+				<a href="/" class="block">
 					<img src={PUBLIC_OBA_LOGO_URL} alt={PUBLIC_OBA_REGION_NAME} class="h-10 rounded-sm" />
+				</a>
+				<a href="/" class="block text-xl font-extrabold">
+					{PUBLIC_OBA_REGION_NAME}
 				</a>
 			</div>
 
@@ -73,43 +56,6 @@
 		</div>
 
 		<div class="md:flex items-center hidden gap-4 px-2 py-2">
-			{#if PUBLIC_OBA_SEARCH_ENABLED === 'true'}
-				<div class="max-w-md mx-auto">
-					<label
-						for="default-search"
-						class="dark:text-white mb-2 text-sm font-medium text-gray-900 sr-only">Search</label
-					>
-					<div class="relative">
-						<div class="start-0 ps-3 absolute inset-y-0 flex items-center pointer-events-none">
-							<svg
-								class="dark:text-gray-400 w-4 h-4 text-gray-500"
-								aria-hidden="true"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 20 20"
-							>
-								<path
-									stroke="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-								/>
-							</svg>
-						</div>
-						<input
-							type="search"
-							id="search"
-							class="bg-gray-50 ps-10 dark:border-gray-600 dark:placeholder-gray-400 block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-full"
-							placeholder="Search a stop or route"
-							required
-							bind:value={searchInput}
-							on:keydown={onHandleSearch}
-						/>
-					</div>
-				</div>
-			{/if}
-
 			<div class="gap-x-4 flex">
 				{#each Object.entries(headerLinks) as [key, value]}
 					<div class="bg-white/80 dark:bg-gray-800 border rounded-md">
@@ -164,7 +110,7 @@
 	</div>
 {/if}
 
-<style>
+<style lang="postcss">
 	.burger-icon,
 	.close-icon {
 		cursor: pointer;
